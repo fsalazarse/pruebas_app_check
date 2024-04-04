f41e710aae4b0eedc554b8251c96ccb6332e8eec
fsalazarse <fsalazar@hyl.cl>
2024-04-04 17:56:58 -0300

    avance

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+  import {onMounted, ref} from 'vue'
+  import listCardView from './components/cards/listCardView.vue'
+  import { initializeApp } from "firebase/app";
+  import {getFirestore, collection, query, getDocs, onSnapshot} from 'firebase/firestore'
+  import {initializeAppCheck, ReCaptchaEnterpriseProvider} from 'firebase/app-check';
+
+
+  const cards = ref([]);
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
+  
+  onMounted(async () => {
+    self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+    initializeAppCheck(app, {
+      provider: new ReCaptchaEnterpriseProvider('6LfKCa4pAAAAAMFS8odz7m2PowDQVQ8Y1YQr8EKW'),
+      //isTokenAutoRefreshEnabled: true // Set 
+    })
+    const q = query(collection(db, "cards"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      cards.value.push(doc.data())
+    });
+
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const cities = [];
+      querySnapshot.forEach((doc) => {
+          cities.push(doc.data().name);
+      });
+      console.log("Current cities in CA: ", cities.join(", "));
+    });
+      })
+
+
+</script>
+
+<template>
+  <h1 class="text-red-500">Hola mundo!!!</h1>
+
+  <listCardView
+    :cards="cards"
+  />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/cards/listCardView.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import {defineProps} from 'vue';
+   
+
+
+    const props = defineProps({
+        cards: {
+            type: Array,
+            required: true
+        }
+    });
+
+
+</script>
+<template>
+    <div>
+        <h1>Lista de tarjetas</h1>
+        {{ props.cards }}
+    </div>  
+</template>
+<style scoped>
+</style>
